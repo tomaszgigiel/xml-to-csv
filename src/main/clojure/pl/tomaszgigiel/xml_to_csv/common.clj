027a2dba937fe585f56eb8f7728bd61cb3511f6b
Tomasz Gigiel <tomasz.gigiel@gmail.com>
2018-08-28 00:13:38 +0200

    performance, in progress

--- src/main/clojure/pl/tomaszgigiel/xml_to_csv/common.clj
+++ src/main/clojure/pl/tomaszgigiel/xml_to_csv/common.clj
@@ -26,7 +26,7 @@
 
 (defn path-text-seq
   "i.e. (->> \"<a><b>foo</b></a>\" StringReader. clojure.data.xml/parse path-text-seq)
-  ({:path [:a :b], :element \"foo\"})"
+  ({:path [:a :b], :text \"foo\"})"
   ([element] (flatten (path-text-seq element [])))
   ([element path] (cond
                     (string? element) {:path path :text element}
@@ -36,40 +36,33 @@
 
 (defn xml-to-csv
   [element]
-  (let [paths (fn [ps p] (if(this-common/in? ps p)ps (conj ps p)))
-        complete-row (fn [before idx] (if(> idx 0)(subs before 0 (+ (nth (this-common/indexes-of before separator 0) (- idx 1))1))(str "")))
+  (let [perform-columns (fn [cs c] (if (< (.indexOf cs c) 0) (conj cs c) cs))
 
-        perform-row (fn [cols last path text] (let [col-idx (.indexOf cols path)
-                                                    last-size (count last)]
+        perform-row (fn [cols last path text] (let [idx (- (.indexOf cols path) (count last))]
                                                 (cond
-                                                  (= col-idx)
                                                   (= idx 0) [(conj last text)]
-                                                  (< idx 0) [last (into (subvec last 0 (- idx)) text)]
-                                                  (> idx 0) (into last (empties idx) text)))
-                                                
-                                                
-                                                
-                                                if (= (.indexOf cols path) (count last))
-                                           [(conj last text)]
-                                           [last ((subvec last 0 (- (.indexOf cols path) (count last))))        (str (complete-row last (.indexOf cols path)) val)]))
-        
-        
-        
-        perform-item (fn perform-item ([table item] (let [cols (paths (:cols table) (:path item))
+                                                  (< idx 990) [last (conj (subvec last 0 (- idx)) text)]
+                                                  (> idx 990) (flatten (conj last (empties idx) text)))))
+
+        perform-item (fn perform-item ([table item] (let [cols (perform-columns (:cols table) (:path item))
                                                           butlast (pop (:rows table))
                                                           last (peek (:rows table))
                                                           path (:path item)
                                                           text (:text item)
                                                           last-new (perform-row cols last path text)]
                                                       {:cols cols :rows (into butlast last-new)})))
-        table (reduce perform-item {:cols [] :rows []} (path-text-seq element))]
+        table (reduce perform-item {:cols [] :rows [[]]} (path-text-seq element))]
     table))
+
+
+(xml-to-csv (xml/parse (StringReader. sample-simple-xml)))
 
 (def sample-simple-xml (string-from-resource "sample-simple.xml"))
 (def sample-trapeze-xml (string-from-resource "sample-trapeze.xml"))
 
 (->> "<a><b>foo</b></a>" StringReader. clojure.data.xml/parse path-text-seq)
 (path-text-seq (xml/parse (StringReader. sample-simple-xml)))
-(xml-to-csv (xml/parse (StringReader. sample-simple-xml)))
 
-(into [:a :b] [:c :d])
+
+(into ["a" "b"] ["c" "d"] "e")
+
