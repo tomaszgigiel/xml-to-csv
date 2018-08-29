16f750670f28caa781dc2a40028ed65092bc5124
Tomasz Gigiel <tomasz.gigiel@gmail.com>
2018-08-29 19:52:37 +0200

    correct path

--- batch/msdos/lein-run-bigger.bat
+++ batch/msdos/lein-run-bigger.bat
@@ -1,4 +1,4 @@
 cd %~dp0/../..
 md "%HOMEPATH%/_delete_content/"
-start lein run-main-xml-to-csv -i "./src/test/resources/bigger.xml" -o "%HOMEPATH%/_delete_content/bigger.csv"
+start lein run-main-xml-to-csv -i "%HOMEPATH%/_delete_content/bigger.xml" -o "%HOMEPATH%/_delete_content/bigger.csv"
 pause
