86905fabccea5c5a135fcbac1a875f31c1e7953b
Tomasz Gigiel <tomasz.gigiel@gmail.com>
2018-09-03 21:45:23 +0200

    xml files with holes

--- src/test/clojure/pl/tomaszgigiel/xml_to_csv/temp_test.clj
+++ src/test/clojure/pl/tomaszgigiel/xml_to_csv/temp_test.clj
@@ -18,3 +18,32 @@
 
 
 (->> "short/f.xml" misc/string-from-resource StringReader. xml/parse path-text-seq)
+(->> "short/f.xml" misc/string-from-resource StringReader. xml/parse)
+
+(def x (->> "short/f.xml" misc/string-from-resource StringReader. xml/parse))
+
+
+#clojure.data.xml.Element{:tag :a, :attrs {}, :content (
+#clojure.data.xml.Element{:tag :b, :attrs {}, :content (
+#clojure.data.xml.Element{:tag :d, :attrs {}, :content ("bb")}
+#clojure.data.xml.Element{:tag :e, :attrs {}, :content ("cc")})}
+#clojure.data.xml.Element{:tag :b, :attrs {}, :content (
+#clojure.data.xml.Element{:tag :c, :attrs {}, :content ("dd")}
+#clojure.data.xml.Element{:tag :d, :attrs {}, :content ("ee")}
+#clojure.data.xml.Element{:tag :e, :attrs {}, :content ("ff")})})}
+
+;;{a nil ab nil abd "bb" abe "cc"}
+;;{a nil ab nil abc "dd" abd "bb" abe "cc"}
+
+(defn enrich [element column]
+  (cond
+    (string? element) {column element}
+    (sequential? element) (const {(:tag element) nil} (map #(enrich % column) element))
+    (map? element) (const {(str (:tag parent) (:tag element)) :content element} (enrich element (str (:tag parent) (:tag element))))
+    :else nil))
+
+(enrich x "")
+
+;;a/b/d;a/b/e;a/b/c
+;;bb;cc;nil
+;;ee;ff;dd
