d53aeebbd8309d330be665b35690488b0f8c75ba
Tomasz Gigiel <tomasz.gigiel@gmail.com>
2018-09-04 22:32:18 +0200

    xml files with holes

--- src/test/clojure/pl/tomaszgigiel/xml_to_csv/temp_test.clj
+++ src/test/clojure/pl/tomaszgigiel/xml_to_csv/temp_test.clj
@@ -1,6 +1,8 @@
 (ns pl.tomaszgigiel.xml-to-csv.temp-test
   (:use [clojure.test])
   (:require [clojure.data.xml :as xml])
+  (:require [clojure.xml :as clojure-xml])
+  (:require [clojure.java.io :as io])
   (:require [clojure.edn :as edn])
   (:require [clojure.string :as string])
   (:require [pl.tomaszgigiel.xml-to-csv.common :as common])
@@ -22,7 +24,6 @@
 
 (def x (->> "short/f.xml" misc/string-from-resource StringReader. xml/parse))
 
-
 #clojure.data.xml.Element{:tag :a, :attrs {}, :content (
 #clojure.data.xml.Element{:tag :b, :attrs {}, :content (
 #clojure.data.xml.Element{:tag :d, :attrs {}, :content ("bb")}
@@ -32,8 +33,51 @@
 #clojure.data.xml.Element{:tag :d, :attrs {}, :content ("ee")}
 #clojure.data.xml.Element{:tag :e, :attrs {}, :content ("ff")})})}
 
+(def clojure-x (->> "short/f.xml" io/resource str clojure-xml/parse))
+
+{:tag :a, :attrs nil, :content [
+{:tag :b, :attrs nil, :content [
+{:tag :d, :attrs nil, :content ["bb"]}
+{:tag :e, :attrs nil, :content ["cc"]}]}
+{:tag :b, :attrs nil, :content [
+{:tag :c, :attrs nil, :content ["dd"]}
+{:tag :d, :attrs nil, :content ["ee"]}
+{:tag :e, :attrs nil, :content ["ff"]}]}]}
+
 ;;{a nil ab nil abd "bb" abe "cc"}
 ;;{a nil ab nil abc "dd" abd "bb" abe "cc"}
+
+;; element is a map
+;; element is a list
+;; element is a string
+
+;; {a ?}
+;; {ab ?} {ab ?}
+;; {a {b ?} {b ?}}
+
+(def xx [":a" [":b" [":d" "bb" ":e" "cc"]] [":b" [":c" "dd" ":d" "ee" ":e" "ff"]]])
+(def xx [":a" "aa"])
+
+(def xx {":a" [{":b" [{":d" "bb"} {":e" "cc"}]} {":b" [{":c" "dd"} {":d" "ee"} {":e" "ff"}]}]})
+;; (([":b:a:d" "bb"] [":b:a:e" "cc"]) ([":b:a:c" "dd"] [":b:a:d" "ee"] [":b:a:e" "ff"]))
+(def xx {":a" "aa"})
+(def xx {":a" [{":b" "bb"} {":c" "cc"}]})
+
+(defn fun-xx [element path]   (cond     (and (map? element) (string? (first (vals element)))) [(str path (first (keys element))) (first (vals element))]
+    (and (map? element) (sequential? (first (vals element)))) (map #(fun-xx % (str (first (keys element)) path)) (first (vals element)))))
+
+(fun-xx xx "")
+(vals xx)
+
+
+(defn fun-xx [element path]   (cond     (sequential? element) (map #(fun-xx % (str (first element) path)) (rest element))
+    (cond
+      (string? (first element))
+      )
+        ))
+
+
+
 
 (defn enrich [element column]
   (cond
