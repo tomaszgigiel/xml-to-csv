ebc67051b5eb3feb35acac21a5da1891ac2a380f
Tomasz Gigiel <tomasz.gigiel@gmail.com>
2018-09-07 21:10:32 +0200

    uninteresting for diff

--- batch/msdos/folder-diff.bat
+++ batch/msdos/folder-diff.bat
@@ -3,5 +3,5 @@ cd %~dp0
 SET old=../../../xml-to-csv-old/
 SET new=../../../xml-to-csv/
 
-diff -x target -x folder-diff.path -x .lein-failures -x .lein-repl-history -x .project -x *.log -ruN %old% %new% > folder-diff.path
+diff -x target -x folder-diff.path -x .lein-failures -x .lein-repl-history -x .nrepl-port -x .project -x *.log -ruN %old% %new% > folder-diff.path
 pause
