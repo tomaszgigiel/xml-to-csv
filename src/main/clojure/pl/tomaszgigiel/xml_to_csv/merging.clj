f73d8037cb017b741c90ee2ceac7b072ce803851
Tomasz Gigiel <tomasz.gigiel@gmail.com>
2018-09-10 22:12:26 +0200

    xml files with holes

--- src/main/clojure/pl/tomaszgigiel/xml_to_csv/merging.clj
+++ src/main/clojure/pl/tomaszgigiel/xml_to_csv/merging.clj
@@ -1,8 +1,39 @@
 (ns pl.tomaszgigiel.xml-to-csv.merging
+  (:require [pl.tomaszgigiel.xml-to-csv.misc :as misc])
   (:use [clojure.set])
   (:gen-class))
 
 (defn cols [x] (set (map :col x)))
+
+(defn row? [l] (and (seq? l) (every? map? l)))
+(defn list-of-rows? [l] (and (seq? l) (every? row? l)))
+(defn list-of-list? [l] (and (seq? l) (every? list? l)))
+
+(defn merge-horizontal? [cols x] (empty? (intersection cols (cols x))))
+(defn merge-vertical? [cols x] ((complement merge-horizontal?) cols x))
+
+(defn merged-horizontal [coll x] (map #(conj coll %) x))
+(defn merged-vertical [coll x] (conj coll x))
+
+(defn merged-ab [a b]
+  (cond
+    (empty? a) b
+    (empty? b) a
+    (list-of-rows? a)
+    (let [cols (cols (first a))]
+      (reduce (fn[a x] (cond
+                         (merge-horizontal? cols x) (merged-horizontal a x)
+                         (merge-vertical? cols x) (merged-vertical a x))) a b))
+    (seq? a)
+    :default "?"))
+
+(defn merged [coll]
+  (cond
+    (empty? coll) ()
+    (list-of-rows? coll) coll
+    (seq? coll)
+    ))
+
 
 (defn core? [l x]
   (let [a (cols (first l))
@@ -15,7 +46,23 @@
 (defn to-core [l] (filter #(core? l %) l))
 (defn to-merge [l] (filter #((complement core?) l %) l))
 
-(defn merged [l]
+(defn merged-2 [l]
   (let [c (to-core l)
         m (to-merge l)]
     (map #(flatten (conj m %)) c)))
+
+
+(row? '({:col 1 :val 1} {:col 2 :val 2}))
+(list-of-rows? '(({:col 1 :val 1} {:col 2 :val 2})({:col 1 :val 3} {:col 2 :val 4})))
+
+
+(def l '(
+      (
+        ({:col "a" :val "a11"} {:col "b" :val "b11"} {:col "c" :val "c11"})
+        ({:col "a" :val "a12"} {:col "b" :val "b12"} {:col "c" :val "c12"})
+        ({:col "a" :val "a13"} {:col "b" :val "b13"} {:col "c" :val "c13"})
+      )
+      {:col "d" :val "d1"}
+    )
+  )
+(merged l)
