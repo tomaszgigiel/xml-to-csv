e0091d55bc9c755073832e62408fffc64f958504
Tomasz Gigiel <tomasz.gigiel@gmail.com>
2018-09-13 23:07:42 +0200

    xml files with holes

--- src/main/clojure/pl/tomaszgigiel/xml_to_csv/merging.clj
+++ src/main/clojure/pl/tomaszgigiel/xml_to_csv/merging.clj
@@ -3,29 +3,28 @@
   (:use [clojure.set])
   (:gen-class))
 
-(defn cols [x] (set (map :col x)))
+(defn get-cols [row] (set (map :col row)))
 
 (defn row? [l] (and (seq? l) (every? map? l)))
 (defn list-of-rows? [l] (and (seq? l) (every? row? l)))
 (defn list-of-list? [l] (and (seq? l) (every? list? l)))
 (defn list-of-rows-and-maps? [coll] (and (seq? coll)(list-of-rows? (first coll))(every? map? (rest coll))))
 
-(defn merge-horizontal? [cols x] (empty? (intersection cols (cols x))))
-(defn merge-vertical? [cols x] ((complement merge-horizontal?) cols x))
+(defn merge-horizontal? [cols row] (do (empty? (intersection cols (get-cols row)))))
+(defn merge-vertical? [cols row] ((complement merge-horizontal?) cols row))
 
 (defn merged-horizontal [a b] (for [x a y b] (flatten (list x y))))
-(defn merged-vertical [coll x] (conj coll x))
+(defn merged-vertical [a b] (reduce  (fn [coll row] (conj coll row)) a b))
 
 (defn merged [coll]
   (cond
     (empty? coll) ()
     (row? coll) (coll)
     (list-of-rows? coll) coll
-    (list-of-list? coll) (merged (map merged coll))
     (list-of-rows-and-maps? coll) (reduce merged-horizontal (first coll) (list (rest coll)))
-
-    (and (seq? coll) (every? list-of-rows? coll)) (let [cols (cols (first coll))]
-                                                    (reduce (fn[c x] (cond
-                                                                        (merge-horizontal? cols x) (merged-horizontal c x)
-                                                                        (merge-vertical? cols x) (merged-vertical c x))) () coll))
+    (and (seq? coll) (every? list-of-rows? coll)) (let [cc (get-cols (first (first coll)))]
+                                                    (reduce (fn[a b] (cond
+                                                                       (merge-horizontal? cc (first b)) (merged-horizontal a b)
+                                                                       (merge-vertical? cc (first b)) (merged-vertical a b))) () coll))
+    (list-of-list? coll) (merged (map merged coll))
     :defult coll))
--- src/test/clojure/pl/tomaszgigiel/xml_to_csv/temp_test.clj
+++ src/test/clojure/pl/tomaszgigiel/xml_to_csv/temp_test.clj
@@ -1,4 +1,5 @@
 (ns pl.tomaszgigiel.xml-to-csv.temp-test
+  (:use [clojure.set])
   (:use [clojure.test])
   (:require [clojure.data.xml :as data-xml])
   (:require [clojure.edn :as edn])
@@ -10,112 +11,47 @@
   (:require [pl.tomaszgigiel.xml-to-csv.test-config :as test-config])
   (:import java.io.StringReader))
 
-(comment
-(
-  ({:col "/aa/bb/a" :val "a1"} {:col "/aa/bb/b" :val "b1"} {:col "/aa/bb/c" :val "c1"})
-  ({:col "/aa/bb/a" :val "a2"} {:col "/aa/bb/b" :val "b2"} {:col "/aa/bb/c" :val "c2"})
-  {:col "/aa/d" :val "d1"}
-  {:col "/aa/d" :val "d2"}
-  ({:col "/aa/bb/a" :val "a3"} {:col "/aa/bb/b" :val "b3"} {:col "/aa/bb/c" :val "c3"})
-  {:col "/aa/d" :val "d3"}
-  ({:col "/aa/bb/e" :val "e1"} {:col "/aa/bb/f" :val "f1"} {:col "/aa/bb/g" :val "g1"})
-  ({:col "/aa/bb/e" :val "e2"} {:col "/aa/bb/f" :val "f2"} {:col "/aa/bb/g" :val "g2"})
-  ({:col "/aa/bb/e" :val "e3"} {:col "/aa/bb/f" :val "f3"} {:col "/aa/bb/g" :val "g3"})
-)
+(def l (->> "l/tree-to-rows-helper.edn" misc/string-from-resource edn/read-string))
 
-(def clojure-i (->> "short/i.xml" io/resource str clojure-xml/parse))
+(defn cols [x] (set (map :col x)))
 
-{
- :tag :aa, :attrs nil, :content [
-                                 {:tag :bb, :attrs nil, :content [
-                                                                  {:tag :a, :attrs nil, :content ["a1"]}
-                                                                  {:tag :b, :attrs nil, :content ["b1"]}
-                                                                  {:tag :c, :attrs nil, :content ["c1"]}
-                                                                  ]
-                                  }
-                                 {:tag :bb, :attrs nil, :content [
-                                                                  {:tag :a, :attrs nil, :content ["a2"]}
-                                                                  {:tag :b, :attrs nil, :content ["b2"]}
-                                                                  {:tag :c, :attrs nil, :content ["c2"]}
-                                                                  ]
-                                  }
-                                 {:tag :d, :attrs nil, :content ["d1"]}
-                                 ]
- }
+(defn row? [l] (and (seq? l) (every? map? l)))
+(defn list-of-rows? [l] (and (seq? l) (every? row? l)))
+(defn list-of-list? [l] (and (seq? l) (every? list? l)))
+(defn list-of-rows-and-maps? [coll] (and (seq? coll)(list-of-rows? (first coll))(every? map? (rest coll))))
+
+(defn merge-horizontal? [cols x] (empty? (intersection cols (cols x))))
+(defn merge-vertical? [cols x] ((complement merge-horizontal?) cols x))
+
+(defn merged-horizontal [a b] (for [x a y b] (flatten (list x y))))
+(defn merged-vertical [a b] (reduce  (fn [coll row] (conj coll row)) a b))
+
+(defn merged [coll]
+  (cond
+    (empty? coll) ()
+    (row? coll) (coll)
+    (list-of-rows? coll) coll
+    (list-of-rows-and-maps? coll) (reduce merged-horizontal (first coll) (list (rest coll)))
+    (and (seq? coll) (every? list-of-rows? coll)) (let [cols (cols (first coll))]
+                                                    (reduce (fn[c x] (merged-vertical c x)) () coll))
+    (list-of-list? coll) (merged (map merged coll))
+    :defult coll))
+
+(merged l)
 
 
-(->> "short/i.xml" misc/string-from-resource data-xml/parse-str)
 
-#clojure.data.xml.Element{:tag :aa, :attrs {}, :content (
-                                                          #clojure.data.xml.Element{:tag :bb, :attrs {}, :content (
-                                                                                                                    #clojure.data.xml.Element{:tag :a, :attrs {}, :content ("a1")}
-                                                                                                                    #clojure.data.xml.Element{:tag :b, :attrs {}, :content ("b1")}
-                                                                                                                    #clojure.data.xml.Element{:tag :c, :attrs {}, :content ("c1")}
-                                                                                                                    )
-                                                                                    }
-                                                          #clojure.data.xml.Element{:tag :bb, :attrs {}, :content (
-                                                                                                                    #clojure.data.xml.Element{:tag :a, :attrs {}, :content ("a2")}
-                                                                                                                    #clojure.data.xml.Element{:tag :b, :attrs {}, :content ("b2")}
-                                                                                                                    #clojure.data.xml.Element{:tag :c, :attrs {}, :content ("c2")}
-                                                                                                                    )
-                                                                                    }
-                                                          #clojure.data.xml.Element{:tag :d, :attrs {}, :content ("d1")}
-                                                          )
-                          }
 
-(def clojure-l (->> "short/l.xml" io/resource str clojure-xml/parse))
+(defn merged [coll]
+  (cond
+    (empty? coll) ()
+    (row? coll) (coll)
+    (list-of-rows? coll) coll
+    (list-of-rows-and-maps? coll) (reduce merged-horizontal (first coll) (list (rest coll)))
+    (and (seq? coll) (every? list-of-rows? coll)) (let [cols (cols (first coll))]
+                                                  (reduce (fn[c x] (cond
+                                                                      (merge-horizontal? cols x) (merged-horizontal c x)
+                                                                      (merge-vertical? cols x) (merged-vertical c x))) () coll))
+    (list-of-list? coll) (merged (map merged coll))
+    :defult coll))
 
-{:tag :aa, :attrs nil, :content [
-                                 {:tag :bb, :attrs nil, :content [
-                                                                  {:tag :cc, :attrs nil, :content [
-                                                                                                   {:tag :dd, :attrs nil, :content [
-                                                                                                                                    {:tag :a, :attrs nil, :content ["a1"]}
-                                                                                                                                    {:tag :b, :attrs nil, :content ["b1"]}
-                                                                                                                                    {:tag :c, :attrs nil, :content ["c1"]}]}
-                                                                                                   {:tag :dd, :attrs nil, :content [
-                                                                                                                                    {:tag :a, :attrs nil, :content ["a2"]}
-                                                                                                                                    {:tag :b, :attrs nil, :content ["b2"]}
-                                                                                                                                    {:tag :c, :attrs nil, :content ["c2"]}]}
-                                                                                                   {:tag :dd, :attrs nil, :content [
-                                                                                                                                    {:tag :a, :attrs nil, :content ["a3"]}
-                                                                                                                                    {:tag :b, :attrs nil, :content ["b3"]}
-                                                                                                                                    {:tag :c, :attrs nil, :content ["c3"]}]}]}
-                                                                  {:tag :d, :attrs nil, :content ["d1"]}
-                                                                  ]
-                                  }
-                                 {:tag :bb, :attrs nil, :content [
-                                                                  {:tag :cc, :attrs nil, :content [
-                                                                                                   {:tag :dd, :attrs nil, :content [
-                                                                                                                                    {:tag :a, :attrs nil, :content ["a1"]}
-                                                                                                                                    {:tag :b, :attrs nil, :content ["b1"]}
-                                                                                                                                    {:tag :c, :attrs nil, :content ["c1"]}]}
-                                                                                                   {:tag :dd, :attrs nil, :content [
-                                                                                                                                    {:tag :a, :attrs nil, :content ["a2"]}
-                                                                                                                                    {:tag :b, :attrs nil, :content ["b2"]}
-                                                                                                                                    {:tag :c, :attrs nil, :content ["c2"]}]}
-                                                                                                   {:tag :dd, :attrs nil, :content [
-                                                                                                                                    {:tag :a, :attrs nil, :content ["a3"]}
-                                                                                                                                    {:tag :b, :attrs nil, :content ["b3"]}
-                                                                                                                                    {:tag :c, :attrs nil, :content ["c3"]}]}]}
-                                                                  {:tag :d, :attrs nil, :content ["d1"]}]}
-                                 {:tag :bb, :attrs nil, :content [
-                                                                  {:tag :cc, :attrs nil, :content [
-                                                                                                   {:tag :dd, :attrs nil, :content [
-                                                                                                                                    {:tag :a, :attrs nil, :content ["a1"]}
-                                                                                                                                    {:tag :b, :attrs nil, :content ["b1"]}
-                                                                                                                                    {:tag :c, :attrs nil, :content ["c1"]}]}
-                                                                                                   {:tag :dd, :attrs nil, :content [
-                                                                                                                                    {:tag :a, :attrs nil, :content ["a2"]}
-                                                                                                                                    {:tag :b, :attrs nil, :content ["b2"]}
-                                                                                                                                    {:tag :c, :attrs nil, :content ["c2"]}]}
-                                                                                                   {:tag :dd, :attrs nil, :content [
-                                                                                                                                    {:tag :a, :attrs nil, :content ["a3"]}
-                                                                                                                                    {:tag :b, :attrs nil, :content ["b3"]}
-                                                                                                                                    {:tag :c, :attrs nil, :content ["c3"]}]}]}
-                                                                  {:tag :d, :attrs nil, :content ["d1"]}
-                                                                  ]
-                                  }
-                                 ]
- }
-
-)
