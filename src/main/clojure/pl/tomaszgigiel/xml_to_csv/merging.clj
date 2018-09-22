dad292ade0973c3a70c35e4d8fdad9504701d03a
Tomasz Gigiel <tomasz.gigiel@gmail.com>
2018-09-22 21:03:09 +0200

    xml files with holes

--- src/main/clojure/pl/tomaszgigiel/xml_to_csv/merging.clj
+++ src/main/clojure/pl/tomaszgigiel/xml_to_csv/merging.clj
@@ -21,7 +21,10 @@
 (defn merge-horizontal? [cols row] (empty? (intersection cols (get-cols row))))
 (defn merge-vertical? [cols row] ((complement merge-horizontal?) cols row))
 
-(defn merged-horizontal [a b] (for [x (to-list-row a) y (to-list-row b)] (flatten (list x y))))
+(defn merged-horizontal [a b] (cond
+                                (empty? a) b
+                                (empty? b) a
+                                :default (for [x (to-list-row a) y (to-list-row b)] (flatten (list x y)))))
 (defn merged-vertical [a b] (reduce (fn [coll row] (conj coll row)) (to-list-row a) (to-list-row b)))
 
 (defn merged-list-of-rows [coll] (let [h (first coll)
@@ -29,10 +32,12 @@
                                        cols (get-cols h)]
                                    (reduce (fn[a b] (if (merge-horizontal? cols b) (merged-horizontal a b) (merged-vertical a b))) h t)))
 
-(defn merged-row-or-map [coll] (let [h (first (filter row? coll))
-                                     t (rest coll)
+(defn merged-row-or-map [coll] (let [splitted (split-with map? coll)
+                                     starting-map (splitted 0)
+                                     h (first (splitted 1))
+                                     t (rest (splitted 1))
                                      cols (get-cols h)]
-                                 (reduce (fn[a b] (if (merge-horizontal? cols b) (merged-horizontal a b) (merged-vertical a b))) h t)))
+                                 (reduce (fn[a b] (if (merge-horizontal? cols b) (merged-horizontal a b) (merged-vertical a (merged-horizontal starting-map b)))) (merged-horizontal starting-map h) t)))
 
 (defn merged-rows-or-map [coll] (let [rs (first (filter rows? coll))
                                       ms (filter map? coll)]
@@ -47,4 +52,4 @@
     (list-of-row-or-map? coll) (merged-row-or-map coll)
     (list-of-rows-or-map? coll) (merged (merged-rows-or-map coll))
     (list-of-list? coll) (merged (map merged coll))
-    :defult "unsupported"))
+    :default "unsupported"))
